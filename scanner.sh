d21c690e6e80e4a4949a930c1f150c1a84c2f9e6
Uriel Ricardo <winchesteruriel@gmail.com>
2019-12-19 11:29:30 -0300

    Create scanner.sh

---
+++ scanner.sh
@@ -0,0 +1,18 @@
+
+PLAYWITHDOCKER ENV:
+	docker pull kalilinux/kali-linux-docker
+	docker run -t -i kalilinux/kali-linux-docker /bin/bash
+	apt-get update && apt-get install metasploit-framework
+
+Use scanner:
+  apt-get install sudo python3 git vim -y
+  git clone https://github.com/kyhau/ssllabs-scan
+  apt-get install curl
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  apt install python3-distutils
+  python3 get-pip.py 
+  apt-get install virtualenv
+  virtualenv env --python=/usr/bin/python3
+  . env/bin/activate
+  pip install -e .
+  ssllabs-scan sample/SampleServerList.txt
