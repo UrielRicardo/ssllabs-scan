f915ac52526ed6cdbb93f71a868e7e868ada65c3
Uriel Ricardo <winchesteruriel@gmail.com>
2020-10-23 21:51:48 -0300

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -8,6 +8,7 @@ Use scanner:
   apt-get install sudo python3 git vim -y
   git clone https://github.com/kyhau/ssllabs-scan
   apt-get install curl
+  apt-get install tree -y
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   apt install python3-distutils
   python3 get-pip.py 
